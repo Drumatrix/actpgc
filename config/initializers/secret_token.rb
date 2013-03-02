c9f00ced0731a35793d2f36a15412f0af1e378d6
David Noack <noackdave@gmail.com>
2013-03-02 17:06:17 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Actpgc::Application.config.secret_token = '14f788ba1dc5f4adb59fbbf33933e378a7b2673859660bf89e6ca7d668a6870833a54c661883dbdff6e53209d6718704590423654693b367b5bcd47382548d93'
